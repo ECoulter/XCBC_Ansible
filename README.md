d325c62f1d1f0de690c1b43121d81f53ae8aa341
jecoulte <jecoulte@iu.edu>
2015-08-20 17:04:01 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Simple playbooks to make XCBC easier
+Ansible handles post-Rocks installation configuration.
+Currently working:
+ -install the XSEDE yum repo for future updates (maybe a bad idea!)
+ -Configure basic Torque queue
---
+++ install_ansible.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+git clone git://github.com/ansible/ansible.git --recursive
+source ./ansible/hacking/env-setup -q
+echo -e "\nsource ./ansible/hacking/env-setup -q" >> .bashrc
+easy_install pip
+pip install paramiko PyYAML Jinja2 httplib2 six
+echo "# Ansible Inventory" > inventory
+echo "[headnode]" >> inventory
+echo "localhost ansible_connection=local" >> inventory
+mkdir /etc/ansible
+mv inventory /etc/ansible/hosts
+./ansible/bin/ansible headnode -a "/bin/hostname"

9402db79cc7255034bbb493aa772f7d6f4554328
jecoulte <jecoulte@iu.edu>
2015-08-21 16:09:10 -0400

    Added pexpect to install; needed for Globus

--- install_ansible.sh
+++ install_ansible.sh
@@ -3,7 +3,7 @@ git clone git://github.com/ansible/ansible.git --recursive
 source ./ansible/hacking/env-setup -q
 echo -e "\nsource ./ansible/hacking/env-setup -q" >> .bashrc
 easy_install pip
-pip install paramiko PyYAML Jinja2 httplib2 six
+pip install paramiko PyYAML Jinja2 httplib2 six pexpect
 echo "# Ansible Inventory" > inventory
 echo "[headnode]" >> inventory
 echo "localhost ansible_connection=local" >> inventory
